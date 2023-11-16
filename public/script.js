301f814b759dfd4a7756be6e0690dd53a2d11b89
Donovan Williams <dw58@email.sc.edu>
2023-11-16 14:23:01 -0500

    server

---
+++ public/script.js
@@ -0,0 +1,211 @@
+const getInstruments = async () => {
+    try {
+        return (await fetch("api/recipes")).json();
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+const showInstruments = async () => {
+    let instruments = await getInstruments();
+    let instrumentsDiv = document.getElementById("recipe-list");
+    instrumentsDiv.innerHTML = "";
+    instruments.forEach((instrument) => {
+        const section = document.createElement("section");
+        section.classList.add("recipe");
+        instrumentsDiv.append(section);
+
+        const a = document.createElement("a");
+        a.href = "#";
+        section.append(a);
+
+        const h3 = document.createElement("h3");
+        h3.innerHTML = instrument.name;
+        a.append(h3);
+
+        if(instrument.img) {
+            const img = document.createElement("img");
+            section.append(img);
+            img.src = instrument.img;
+        }
+        
+
+        a.onclick = (e) => {
+            e.preventDefault();
+            displayDetails(instrument);
+        };
+    });
+};
+
+const displayDetails = (instrument) => {
+    const instrumentDetails = document.getElementById("recipe-details");
+    instrumentDetails.innerHTML = "";
+
+    const h3 = document.createElement("h3");
+    h3.innerHTML = instrument.name;
+    instrumentDetails.append(h3);
+
+    const dLink = document.createElement("a");
+    dLink.innerHTML = "&#x2715;"
+    instrumentDetails.append(dLink);
+    dLink.id = "delete-link";
+
+    const eLink = document.createElement("a");
+    eLink.innerHTML = "&#9998;"
+    instrumentDetails.append(eLink);
+    eLink.id = "edit-link";
+    
+    const p = document.createElement("p");
+    instrumentDetails.append(p);
+    p.innerHTML = "<b>Instrument Desciption:</b> " + instrument.description;
+
+    const p3 = document.createElement("p");
+    instrumentDetails.append(p3);
+    p3.innerHTML = "<b>Instrument Material:</b> " + instrument.material;
+
+    const p2 = document.createElement("p");
+    instrumentDetails.append(p2);
+    p2.innerHTML = "<b>Instrument Parts:</b> " + instrument.parts;
+
+    eLink.onclick = (e) => {
+        e.preventDefault();
+        document.querySelector(".dialog").classList.remove("transparent");
+        document.getElementById("add-edit-title").innerHTML = "Edit Instrument";
+    }
+
+    dLink.onclick = (e) => {
+        e.preventDefault();
+        deleteInstrument(instrument);
+    }
+
+    populateEditForm(instrument);
+};
+
+const deleteInstrument = async (instrument) => {
+    let response = await fetch(`/api/recipes/${instrument.id}`, {
+        method: "DELETE", 
+        headers: {
+            "Content-Type": "application/json;charset=utf-8",
+        },
+    });
+
+    if (response.status != 200) {
+        console.log("error deleting");
+        return;
+    }
+
+    let result = await response.json();
+    showInstruments();
+    document.getElementById("recipe-details").innerHTML = "";
+    resetForm();
+};
+
+const populateEditForm = (instrument) => {
+    const form = document.getElementById("add-edit-instrument-form");
+    form._id.value = instrument.id;
+    form.name.value = instrument.name;
+    form.description.value = instrument.description;
+    form.material.value = instrument.material;
+
+    //add ingredients 
+    populateParts(instrument.parts);
+};
+
+const populateParts = (parts) => {
+    const partBoxes = document.getElementById("part-boxes");
+    parts.forEach((part) => {
+        const input = document.createElement("input");
+        input.type = "text";
+        input.value = part;
+        partBoxes.append(input);
+    });
+};
+
+const addEditInstrument = async (e) => {
+    e.preventDefault();
+
+    const form = document.getElementById("add-edit-instrument-form");
+    const formData = new FormData(form);
+    // formData.delete("img");
+    formData.append("parts", getParts());
+    // console.log(...formData);
+
+    let response;
+    //add new instrument
+    if(form._id.value == -1) {
+        formData.delete("_id");
+        // console.log(...formData);
+
+        response = await fetch("/api/recipes", {
+            method: "POST",
+            body: formData,
+        });
+    } else {
+        // existing instrument
+        response = await fetch(`/api/recipes/${form._id.value}`, {
+            method: "PUT",
+            body: formData,
+        });
+        // console.log("edit mode");
+        // console.log(...formData);
+    }
+
+    if(response.status != 200) {
+        console.log("Error contacting server");
+        return;
+    }
+    instrument = await response.json();
+   
+    //edit mode
+    if (form._id.value != -1) {
+        displayDetails(instrument);
+    };
+
+    document.querySelector(".dialog").classList.add("transparent");
+    resetForm();
+    showInstruments();
+};
+
+const getParts = () => {
+    const inputs = document.querySelectorAll("#part-boxes input");
+    const parts = [];
+
+    inputs.forEach((input)=> {
+        parts.push(input.value);
+    });
+    return parts;
+}
+
+const resetForm = () => {
+    const form = document.getElementById("add-edit-instrument-form");
+    form.reset();
+    form._id ="-1";
+    document.getElementById("part-boxes").innerHTML = "";
+};
+
+const showHideAdd = (e) => {
+    e.preventDefault();
+    document.querySelector(".dialog").classList.remove("transparent");
+    document.getElementById("add-edit-title").innerHTML = "Add A New Instrument";
+    resetForm();
+};
+
+const addPart = (e) => {
+    e.preventDefault();
+    const partBoxes = document.getElementById("part-boxes");
+    const input = document.createElement("input");
+    input.type = "text";
+    partBoxes.append(input);
+};
+
+window.onload = () => {
+    showInstruments();
+    document.getElementById("add-edit-instrument-form").onsubmit = addEditInstrument;
+    document.getElementById("add-link").onclick = showHideAdd;
+
+    document.querySelector(".close").onclick = () => {
+        document.querySelector(".dialog").classList.add("transparent");
+    };
+
+    document.getElementById("add-part").onclick = addPart;
+};
---
+++ server.js
@@ -0,0 +1,204 @@
+const express = require("express");
+const app = express();
+const Joi = require("joi");
+const multer = require("multer");
+app.use(express.static("public"));
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+
+const upload = multer({ dest: __dirname + "/public/images" });
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+
+let instruments = [
+    {
+        id: 1,
+        name: "Piano",
+        description: "A delicate instrument that creates sound by hammers hitting strings",
+        material: "Maple wood",
+        img: "images/piano.jpg",
+        parts: [
+            "Keys",
+            "Strings",
+            "Pedals",
+            "Hammers",
+            "Dampers",
+            "Frame",
+        ],
+    },
+    { 
+        id: 2,
+        name: "Guitar",
+        description: "A stringed instrument",
+        material: "Spruce wood",
+        img: "images/guitar.jpg",
+        parts: [
+            "Strings",
+            "Frets",
+            "Fretboard",
+            "Tuners",
+            "Stringpost",
+            "Neck"
+        ],
+    },
+    { 
+        id: 3,
+        name: "Drumset",
+        description: "An instrument that creates rythms and holds the beat",
+        material: "Birch wood",
+        img: "images/drum.jpg",
+        parts: [
+            "Snare drum",
+            "Hi-hat",
+            "Crash cymbal",
+            "Ride Cymbal",
+            "Low tom",
+            "High Tom",
+        ],
+    },
+    { 
+        id: 4,
+        name: "Saxophone",
+        description: "A woodwind instrument",
+        material: "Brass",
+        img: "images/sax.jpg",
+        parts: [
+            "Neck",
+            "Body",
+            "Reed",
+            "Bell",
+            "Mouth Piece",
+            "Key Pearls",
+        ],
+    },
+    { 
+        id: 5,
+        name: "Xylophone",
+        description: "A percussion instrument made of wood",
+        material: "Rosewood",
+        img: "images/exylo.jpg",
+        parts: [
+            "Bars",
+            "Resonating tubes",
+            "Mallets",
+            "Wheels",
+            "Wood",
+            "Keys",
+        ],
+    },
+    {
+        id: 6,
+        name: "Trumpet",
+        description: "A brass instrument",
+        material: "Brass",
+        img: "images/trumpet.jpg",
+        parts: [
+            "Mouth piece",
+            "Lead pipe",
+            "Bell",
+            "Tuning slide",
+            "Pistons",
+            "Valves",
+        ],
+    },
+];
+
+app.get("/api/recipes", (req, res) => {
+    res.send(instruments);
+});
+
+app.get("/api/recipes/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+  
+    const instrument = instruments.find((r)=>r.id === id);
+
+    if (!instrument) {
+        res.status(404).send("The recipe with the given id was not found");
+    }
+
+    res.send(instrument);
+});
+
+app.post("/api/recipes", upload.single("img"), (req,res) => {
+    const result = validateInstrument(req.body);
+    if(result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    const instrument = {
+        _id: instruments.length + 1,
+        name: req.body.name,
+        description: req.body.description,
+        material: req.body.material,
+        parts: req.body.parts.split(","),
+    };
+
+    // if (req.body.parts) {
+    //     instrument.parts = req.body.parts.split(",");
+    // }
+
+    if (req.file) {
+        instrument.img = "images/" + req.file.filename;
+    }
+
+    instruments.push(instrument);
+    res.send(instrument);
+});
+
+app.put("/api/recipes/:id", upload.single("img"), (req, res) => {
+    const id = parseInt(req.params.id);
+  
+    const instrument = instruments.find((r)=>r.id === id);
+
+    const result = validateInstrument(req.body);
+
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    instrument.name = req.body.name;
+    instrument.description = req.body.description;
+    instrument.material = req.body.material;
+    instrument.parts = req.body.parts.split(",");
+
+     if (req.file) {
+        instrument.img = "images/" + req.file.filename;
+    }
+
+    res.send(instrument);
+});
+
+app.delete("/api/recipes/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+  
+    const instrument = instruments.find((r)=>r.id === id);
+
+    if (!instrument) {
+        res.status(404).send("The recipe with the given id was not found");
+    }
+
+    const index = instruments.indexOf(instrument);
+    instruments.splice(index, 1);
+    res.send(instrument);
+});
+
+const validateInstrument = (instrument) => {
+    const schema = Joi.object({
+        _id: Joi.allow(""),
+        name: Joi.string().min(3).required(),
+        description: Joi.string().min(3).required(),
+        material: Joi.string().min(3).required(),
+        parts: Joi.allow(""),
+    });
+
+    return schema.validate(instrument);
+};
+
+app.listen(3000, () => {
+    console.log("I'm listening");
+});
